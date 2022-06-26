a450877152ddba80e18648c22555dfc1bc867df6
govrial <36007441+govrial@users.noreply.github.com>
2022-06-26 19:22:50 +0300

    #commot

---
+++ js/app.js
@@ -0,0 +1,31 @@
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+    output.innerHTML = this.value;
+}
+
+let inputs = document.querySelectorAll('.input__file');
+Array.prototype.forEach.call(inputs, function (input) {
+    let label = input.nextElementSibling,
+        labelVal = label.querySelector('.input__file-button-text').innerText;
+
+    input.addEventListener('change', function (e) {
+        let countFiles = '';
+        if (this.files && this.files.length >= 1)
+            countFiles = this.files.length;
+
+        if (countFiles)
+            label.querySelector('.input__file-button-text').innerText = 'Выбрано файлов: ' + countFiles;
+        else
+            label.querySelector('.input__file-button-text').innerText = labelVal;
+    });
+});
+
+
+$('.menu-burger').on('click', function () {
+    $(this).toggleClass('menu-burger_active');
+    $('.navbar').toggleClass('navbar__active');
+    $('body').removeClass('fixed-header');
+})
